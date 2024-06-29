8157768f2f1f98fc32de2ed4bb56e2712b23ef35
Marco Dominguez <47532404+MarcoDomi@users.noreply.github.com>
2024-06-29 15:22:20 -0500

    add function that converts image to ascii art

--- ascii_convert.py
+++ ascii_convert.py
@@ -1,4 +1,31 @@
 from PIL import Image
-im = Image.open('smile.jpg')
+from PIL import ImageOps
 
-im.show()
+def create_ascii_image(img, f):
+    width, height = img.size
+
+    for i in range(height):
+        for j in range(width):
+            pixel_value = img.getpixel((j, i))
+            if pixel_value in range(50):
+                f.write("*")
+            elif pixel_value in range(50, 100):
+                f.write("#")
+            elif pixel_value in range(100, 150):
+                f.write("$")
+            elif pixel_value in range(150, 200):
+                f.write("%")
+            elif pixel_value in range(200, 255):
+                f.write("@")
+        f.write('\n')
+
+
+im = Image.open('monalisa.jpg')
+size = im.size
+
+gray_im = ImageOps.grayscale(im)
+
+f = open("ascii_image.txt", 'w')
+create_ascii_image(gray_im, f)
+im.close()
+f.close()
